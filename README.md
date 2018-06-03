a4ab080da0a582fbcd3001d0eda5ee43d0828701
sttk <sttk.xslet@gmail.com>
2018-06-03 13:04:16 +0900

    Add node v10 to ci test target and checked versions, and remove v0.7 from checked versions

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ To install from npm:
 $ npm install --save @fav/type.format-number
 ```
 
-***NOTE:*** *npm < 2.7.0 does not support scoped package, but even old version Node.js supports it. So when you use such older npm, you should download this package from [github.com][repo-url], and move it in `node_modules/@fav/type.format-number/` directory manually.*
+***NOTE:*** *npm < 2.7.0 does not support scoped package, but old version Node.js supports it. So when you use such older npm, you should download this package from [github.com][repo-url], and move it in `node_modules/@fav/type.format-number/` directory manually.*
 
 
 ## Usage
@@ -99,14 +99,13 @@ A formatted string which represents the specified date.
 
 
 ## Checked                                                                      
+### Node.js (4〜)
 
-### Node.js (4〜9)
-
-| Platform  |   4    |   5    |   6    |   7    |   8    |   9    |
-|:---------:|:------:|:------:|:------:|:------:|:------:|:------:|
-| macOS     |&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|
-| Windows10 |&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|
-| Linux     |&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|
+| Platform  |   4    |   5    |   6    |   7    |   8    |   9    |   10   |
+|:---------:|:------:|:------:|:------:|:------:|:------:|:------:|:------:|
+| macOS     |&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|
+| Windows10 |&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|
+| Linux     |&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|
 
 ### io.js (1〜3)
 
@@ -118,11 +117,11 @@ A formatted string which represents the specified date.
 
 ### Node.js (〜0.12)
 
-| Platform  |  0.7   |  0.8   |  0.9   |  0.10  |  0.11  |  0.12  |
-|:---------:|:------:|:------:|:------:|:------:|:------:|:------:|
-| macOS     |        |&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|
-| Windows10 |        |&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|
-| Linux     |&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|
+| Platform  |  0.8   |  0.9   |  0.10  |  0.11  |  0.12  |
+|:---------:|:------:|:------:|:------:|:------:|:------:|
+| macOS     |&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|
+| Windows10 |&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|
+| Linux     |&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|&#x25ef;|
 
 ### Web browsers
 
@@ -135,7 +134,7 @@ A formatted string which represents the specified date.
 
 ## License
 
-Copyright (C) 2017 Takayuki Sato
+Copyright (C) 2017-2018 Takayuki Sato
 
 This program is free software under [MIT][mit-url] License.
 See the file LICENSE in this distribution for more details.
--- web/fav.type.format-number.js
+++ web/fav.type.format-number.js
@@ -1,4 +1,4 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g=(g.fav||(g.fav = {}));g=(g.type||(g.type = {}));g.formatNumber = f()}})(function(){var define,module,exports;return (function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g=(g.fav||(g.fav = {}));g=(g.type||(g.type = {}));g.formatNumber = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 'use strict';
 
 var isString = require('@fav/type.is-string');
@@ -319,13 +319,11 @@ module.exports = isString;
 },{}],13:[function(require,module,exports){
 'use strict';
 
-var isString = require('@fav/type.is-string');
 var isFiniteNumber = require('@fav/type.is-finite-number');
+var toNumber = require('@fav/type.to-number');
 
 function toInteger(value) {
-  if (isString(value)) {
-    value = parseFloat(value);
-  }
+  value = toNumber(value);
 
   if (isFiniteNumber(value)) {
     return value < 0 ? Math.ceil(value) : Math.floor(value);
@@ -340,5 +338,33 @@ function toInteger(value) {
 
 module.exports = toInteger;
 
-},{"@fav/type.is-finite-number":10,"@fav/type.is-string":12}]},{},[1])(1)
+},{"@fav/type.is-finite-number":10,"@fav/type.to-number":14}],14:[function(require,module,exports){
+'use strict';
+
+var isString = require('@fav/type.is-string');
+
+function toNumber(value) {
+  if (typeof value === 'number') {
+    if (value === value) {
+      return value;
+    }
+
+  } else if (isString(value)) {
+    if (value && !/ /.test(value)) {
+      value = Number(value);
+      if (value === value) {
+        return value;
+      }
+    }
+  }
+
+  if (arguments.length > 1) {
+    return arguments[1];
+  }
+  return NaN;
+}
+
+module.exports = toNumber;
+
+},{"@fav/type.is-string":12}]},{},[1])(1)
 });
